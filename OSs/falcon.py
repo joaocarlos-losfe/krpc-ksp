fc504972e79c22b6c52c9695e6140603bff5d063
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2023-09-05 22:24:35 -0300

    maxq e deorbita adicionado

--- OSs/falcon.py
+++ OSs/falcon.py
@@ -20,15 +20,17 @@ class FalconOS:
     _ALTITUDE_DO_PERIATRO_ORBITAL = _conn.add_stream(getattr, _info_da_orbita, 'periapsis_altitude')
     _VELOCIDADE_DE_SURPEFICIE = _conn.add_stream(getattr, _info_voo, 'speed')
     _ESTAGIO_ATUAL = _conn.add_stream(getattr, _info_controles, 'current_stage')
-
+    _INCLINACAO_HORIZONTAL = _conn.add_stream(getattr, _info_voo, 'pitch')
     # ações do voo
-    _incrementar_inclinacao_horizontal = 00.00
-    _encerrou_giro_orbital = False
+    _inclinacao_horizontal = 00.00
+    _alcancou_max_q = False
+    _passou_do_max_q = False
     _alcancou_apostro_futuro = False
     _alcancou_apoatro = False
     _liberou_coifa = False
     _alcançou_periastro = False
     _liberou_carga = False
+    _iniciou_deorbita = False
     _ALTITUDE_DO_APOSTRO_PARA_VOO = 110 * 1000 # cento e dez mil quilomentros
     _ALTITUDE_DO_PERIASTRO_PARA_VOO = 70 * 1000 # setenta mil quilometros
 
@@ -46,10 +48,23 @@ class FalconOS:
            self._visualizar_dados_da_telemetria()
            self._giro_gravitacional()
            self._separar_estagios()
+           self._maxq()
            self._checar_apoastro_futuro()
            self._religar_motor_apos_chegar_no_apoastro()
            self._desligar_motor_apos_atingir_periatro()
            self._liberar_carga()
+           self._deorbitar()
+
+    def _deorbitar(self):
+        if not self._iniciou_deorbita and self._liberou_carga:
+            time.sleep(10)
+            self._vessel.auto_pilot.target_pitch_and_heading(180, 90)  #90 90  
+            self._iniciou_deorbita = True
+            time.sleep(10)
+            self._vessel.control.throttle = 1
+            time.sleep(5)
+            self._vessel.control.throttle = 0
+
 
     def _liberar_carga(self):
         if not self._liberou_carga and self._alcançou_periastro:
@@ -57,7 +72,7 @@ class FalconOS:
             self._vessel.control.solar_panels = True
             self._liberou_carga = True
             time.sleep(4)
-            self._vessel.auto_pilot.target_pitch_and_heading(90, 00)    
+            self._vessel.auto_pilot.target_pitch_and_heading(0, 90)    
             self._vessel.control.activate_next_stage()
 
 
@@ -71,9 +86,9 @@ class FalconOS:
     def _religar_motor_apos_chegar_no_apoastro(self):
             if self._ALTITUDE_ATUAL() >= self._ALTITUDE_DO_APOSTRO_PARA_VOO \
             and not self._alcancou_apoatro:
-                self._vessel.auto_pilot.target_direction = (90.00, 00.00, 00.00) #inclinação para o horizonte
-                time.sleep(1)
-                self._vessel.control.throttle = 0.9
+                self._vessel.auto_pilot.target_pitch_and_heading(-10, 90)  #90 90  
+                time.sleep(5)
+                self._vessel.control.throttle = 0.6
                 self._alcancou_apoatro = True
    
     def _checar_apoastro_futuro(self):
@@ -92,15 +107,21 @@ class FalconOS:
         elif not self._liberou_coifa and self._ALTITUDE_ATUAL() >= 70500:
             self._vessel.control.activate_next_stage()
             self._liberou_coifa = True
+    
+    def _maxq(self):
+        if not self._alcancou_max_q and self._ALTITUDE_ATUAL() >= 7500 and self._ALTITUDE_ATUAL() <= 12500 and not self._passou_do_max_q:
+            self._vessel.control.throttle = 0.8
+            self._alcancou_max_q = True
+        elif self._alcancou_max_q and not self._passou_do_max_q and self._ALTITUDE_ATUAL() > 12500:
+            self._vessel.control.throttle = 1
+            self._passou_do_max_q = True
 
+            
     def _giro_gravitacional(self):
-        if self._ALTITUDE_ATUAL() > 5500 and not self._encerrou_giro_orbital:
-            self._vessel.auto_pilot.target_direction = (90.00, 00.00, self._incrementar_inclinacao_horizontal)
-            self._incrementar_inclinacao_horizontal += 0.3 
+        if self._ALTITUDE_ATUAL() > 5500 and not self._alcancou_apoatro:
+            self._vessel.auto_pilot.target_direction = (90.00, 00.00, self._inclinacao_horizontal)
+            self._inclinacao_horizontal += 0.3 
         
-        if self._alcancou_apoatro and not self._encerrou_giro_orbital:
-            self._encerrou_giro_orbital = True
-
             
     def _visualizar_dados_da_telemetria(self):
         print(f'Força G: {self._FORCA_G()}')
@@ -108,9 +129,8 @@ class FalconOS:
         print(f'Altitude orbital {self._ALTITUDE_DO_APOASTRO_ORBITAL()}') 
         print(f'Altitude do periastro {self._ALTITUDE_DO_PERIATRO_ORBITAL()}') 
         print(f'Velocidade orbital: {self._VELOCIDADE_ORBITAL()}')
-        print(f'Estágio atual: {self._ESTAGIO_ATUAL()}°\n')
-
-    
+        print(f'Estágio atual: {self._ESTAGIO_ATUAL()}º')
+        print(f'Inclinação horizontal {self._INCLINACAO_HORIZONTAL()}°')    
     
     def _testar_sistemas(self):
         self._vessel.control.rcs = True
@@ -125,13 +145,13 @@ class FalconOS:
             time.sleep(1)
 
         self._vessel.control.rcs = False
-        self._vessel.auto_pilot.target_pitch_and_heading(90, 90)    
+        self._vessel.auto_pilot.target_pitch_and_heading(90, 90)  #90 90  
 
     
     def _contagem_regressiva(self):
         print('Contagem regresiva')
 
-        for x in range(10, 0, -1):
+        for x in range(2, 0, -1):
             print(f'Lançamento em ... {x}')
             time.sleep(1)
             os.system('cls')
